b3300d62eb66f115dcdc1b7a15494a99eacbef73
Saraah Cooper <saraah.cooper@aa.com>
2024-11-12 11:58:43 -0600

    got the basics for saraah site

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,97 @@
-import Image from "next/image";
-import { Box, Card, Center, CardHeader, CardBody, CardFooter, Text, Flex, theme } from '@chakra-ui/react'
-import WelcomeMessage from "./components/welcome";
-import Data from "./components/data";
-import DataTable from "./components/dashboard/dataTable";
-import { darkMode } from "./theme/colors";
+import { Text, Box, Center, Flex, Image } from "@chakra-ui/react";
 
 export default function Home() {
+  // background="tomato" width="100%" padding="4" color="white
   return (
-    <Box >
-      <Box mt={100} mb={25}>
-        <WelcomeMessage/>
+    <>
+      {/* Title */}
+      <Box background="tomato" padding="4">
+        <Center>
+          <Text>Saraah</Text>
+        </Center>
       </Box>
-      
-      <Box mt={25} p={10}>
-        <Data/>
+      {/* Experences */}
+      <Box background="red" padding="4">
+        <Center>
+          <Flex>
+            <Box background="pink" padding="4">
+              <Text>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Viverra
+                facilisi vivamus laoreet malesuada molestie ut imperdiet quam.
+                Sollicitudin habitasse mi nec sodales nisi. Imperdiet duis
+                hendrerit convallis fames purus congue efficitur posuere.
+              </Text>
+            </Box>
+            <Box background="black" padding="4">
+              <Text>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Viverra
+                facilisi vivamus laoreet malesuada molestie ut imperdiet quam.
+                Sollicitudin habitasse mi nec sodales nisi. Imperdiet duis
+                hendrerit convallis fames purus congue efficitur posuere.
+              </Text>
+            </Box>
+            <Box background="gray" padding="4">
+              <Text>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Viverra
+                facilisi vivamus laoreet malesuada molestie ut imperdiet quam.
+                Sollicitudin habitasse mi nec sodales nisi. Imperdiet duis
+                hendrerit convallis fames purus congue efficitur posuere.
+              </Text>
+            </Box>
+          </Flex>
+        </Center>
       </Box>
-
-      <Box>
-        <DataTable></DataTable>
+      {/* Activites */}
+      <Box background="orange" padding="4">
+        <Box background="tomato" padding="4">
+          <Center>
+            <Text>Activities </Text>
+          </Center>
+        </Box>
+        <Box background="red" padding="4">
+          <Center>
+            <Flex>
+              <Box background="brown" padding="4">
+                <Image
+                  src="https://bit.ly/naruto-sage"
+                  boxSize="150px"
+                  borderRadius="full"
+                  fit="cover"
+                  alt="Naruto Uzumaki"
+                />
+              </Box>
+              <Box background="pink" padding="4">
+                <Image
+                  src="https://bit.ly/naruto-sage"
+                  boxSize="150px"
+                  borderRadius="full"
+                  fit="cover"
+                  alt="Naruto Uzumaki"
+                />
+              </Box>
+              <Box background="black" padding="4">
+                <Image
+                  src="p2.png"
+                  boxSize="150px"
+                  borderRadius="full"
+                  fit="cover"
+                  alt="Naruto Uzumaki"
+                />
+              </Box>
+              <Box background="gray" padding="4">
+                <Image
+                  src="https://bit.ly/naruto-sage"
+                  boxSize="150px"
+                  borderRadius="full"
+                  fit="cover"
+                  alt="Naruto Uzumaki"
+                />
+              </Box>
+            </Flex>
+          </Center>
+        </Box>
       </Box>
-      
-    </Box>
+      {/* <Text>Saraah</Text> */}
+    </>
   );
 }
